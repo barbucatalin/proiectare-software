12cfd7f58ada8731845f268fe5226533541a08b4
Catalin <catalinflorin.barbu@ulbsibiu.ro>
2026-03-27 15:45:40 +0200

    Tema laborator 4

--- src/laborator4/AppLab4.java
+++ src/laborator4/AppLab4.java
@@ -5,7 +5,6 @@ import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-
 class Tanar {
     private String name;
     private int age;
@@ -25,8 +24,22 @@ class Tanar {
 
 public class AppLab4 {
 
-    public static void main(String[] args) {
+    public static float gasesteNota(String prenume, String nume, Map<String, Student> mapInitial) {
+        HashMap<String, Student> cautareDupaNume = new HashMap<>();
 
+        for (Student s : mapInitial.values()) {
+            cautareDupaNume.put(s.getPrenume() + " " + s.getNume(), s);
+        }
+
+        String cheieCautata = prenume + " " + nume;
+        if (cautareDupaNume.containsKey(cheieCautata)) {
+            return cautareDupaNume.get(cheieCautata).getNota();
+        }
+
+        return 0.0f;
+    }
+
+    public static void main(String[] args) {
         HashMap<String, Integer> varste = new HashMap<>();
         varste.put("Ioan", 21);
         varste.put("Maria", 22);
@@ -35,29 +48,19 @@ public class AppLab4 {
         varste.put("Marius", 21);
         varste.put("Mihai", 21);
         varste.put("Daniela", 23);
-
-
         varste.put("Vlad", 19);
         varste.put("Iulia", 19);
-
 
         Map<String, String> adrese = Map.of(
                 "Ioan", "Sibiu", "Maria", "Bucuresti", "Victor", "Cluj",
                 "Simina", "Alba-Iulia", "Marius", "Medias", "Mihai", "Cisnadie", "Daniela", "Sibiu"
         );
 
-
         HashMap<String, Tanar> tineri = new HashMap<>();
         for (String nume : varste.keySet()) {
             String adresa = adrese.getOrDefault(nume, "Necunoscuta");
             tineri.put(nume, new Tanar(nume, varste.get(nume), adresa));
         }
-
-
-        System.out.println("--- Lista Tineri (4.5.1) ---");
-        tineri.forEach((k, v) -> System.out.println(k + " -> " + v));
-
-
 
         List<Student> listaInitiala = new ArrayList<>();
         listaInitiala.add(new Student("1024", "Ioan", "Mihalcea", "ISM141/1"));
@@ -67,15 +70,12 @@ public class AppLab4 {
         listaInitiala.add(new Student("1028", "Mihai", "Ionescu", "TI132/1"));
         listaInitiala.add(new Student("1029", "Bianca", "Popescu", "TI131/1"));
 
-
         HashMap<String, Student> mapStudenti = new HashMap<>();
         for (Student s : listaInitiala) {
             mapStudenti.put(s.getNumarMatricol(), s);
         }
 
-
         try {
-
             File fileNote = new File("src/laborator4/note_anon.txt");
             Scanner sc = new Scanner(fileNote);
             while (sc.hasNextLine()) {
@@ -83,8 +83,6 @@ public class AppLab4 {
                 if (date.length == 2) {
                     String matricol = date[0].trim();
                     float nota = Float.parseFloat(date[1].trim());
-
-
                     if (mapStudenti.containsKey(matricol)) {
                         mapStudenti.get(matricol).setNota(nota);
                     }
@@ -92,13 +90,14 @@ public class AppLab4 {
             }
             sc.close();
         } catch (FileNotFoundException e) {
-            System.out.println("\n[Eroare] Fisierul note_anon.txt nu a fost gasit!");
+            System.out.println("Eroare: Fisierul note_anon.txt nu a fost gasit!");
         }
 
+        System.out.println("--- Rezultate Cautare ---");
+        float notaM = gasesteNota("Bianca", "Popescu", mapStudenti);
+        float notaN = gasesteNota("Ioan", "Popa", mapStudenti);
 
-        System.out.println("\n--- Lista Studenti cu Note (4.5.2) ---");
-        for (Student s : mapStudenti.values()) {
-            System.out.println(s);
-        }
+        System.out.println("Nota pentru Bianca Popescu: " + notaM);
+        System.out.println("Nota pentru Ioan Popa: " + notaN);
     }
 }
